12171fee1bc354e4663c79610fdeae6a9e94a4ce
Logan <loganbentley@gmail.com>
2022-09-29 23:24:30 +0200

    Fix purge bug

--- rs/ai/requested_strike/handlers/purge_handler.py
+++ rs/ai/requested_strike/handlers/purge_handler.py
@@ -4,7 +4,6 @@ from rs.machine.command import Command
 from rs.machine.handlers.handler import Handler
 from rs.machine.state import GameState
 
-
 class PurgeHandler(Handler):
 
     def __init__(self):
@@ -19,7 +18,7 @@ class PurgeHandler(Handler):
         return state.has_command(Command.CHOOSE) \
                and state.game_state()["screen_type"] == "GRID" \
                and state.game_state()["screen_state"]["for_purge"] \
-               and self.get_choice(state.game_state()) > -1
+               and self.get_choice(state) > -1
 
     def handle(self, state: GameState) -> List[str]:
         return ["wait 30", "choose " + str(self.get_choice(state)), "wait 30", "proceed", "wait 30"]
