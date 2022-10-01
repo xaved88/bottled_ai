1b4420e6d0e0b38de553d9ebd551d75796f6e195
Logan <loganbentley@gmail.com>
2022-10-01 23:44:05 +0200

    Fix empty cage bug

--- rs/ai/requested_strike/handlers/purge_handler.py
+++ rs/ai/requested_strike/handlers/purge_handler.py
@@ -4,6 +4,7 @@ from rs.machine.command import Command
 from rs.machine.handlers.handler import Handler
 from rs.machine.state import GameState
 
+
 class PurgeHandler(Handler):
 
     def __init__(self):
@@ -18,14 +19,28 @@ class PurgeHandler(Handler):
         return state.has_command(Command.CHOOSE) \
                and state.game_state()["screen_type"] == "GRID" \
                and state.game_state()["screen_state"]["for_purge"] \
-               and self.get_choice(state) > -1
+               and len(self.get_choices(state)) > -1
 
     def handle(self, state: GameState) -> List[str]:
-        return ["wait 30", "choose " + str(self.get_choice(state)), "wait 30", "proceed", "wait 30"]
+        commands = []
+        amount = 1
+        if 'screen_state' in state.game_state() and 'num_cards' in state.game_state()['screen_state']:
+            amount = state.game_state()['screen_state']['num_cards']
+        for c in self.get_choices(state)[0:amount]:
+            commands.append("choose " + str(c))
+            commands.append("wait 30")
+        return commands
 
-    def get_choice(self, state: GameState) -> int:
-        choices = state.get_choice_list()
+    def get_choices(self, state: GameState) -> List[int]:
+        choice_list = state.get_choice_list()
+        choices = []
         for pref in self.preferences:
-            if pref in choices:
-                return choices.index(pref)
-        return -1
+            for i in range(len(choice_list)):
+                if pref == choice_list[i]:
+                    choices.append(i)
+
+        for i in range(len(choice_list)):
+            if i not in choices:
+                choices.append(i)
+
+        return choices
