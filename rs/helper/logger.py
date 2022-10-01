144a7db6b9ce2021838b56425893016c6f8e5416
Logan <loganbentley@gmail.com>
2022-10-01 21:04:08 +0200

    Make file paths mac friendly :)

--- rs/helper/logger.py
+++ rs/helper/logger.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-ROOT_DIR = ".\\ai\\requested_strike"
+ROOT_DIR = "./ai/requested_strike"
 
 current_run_log_file: str = ''
 current_run_log_count: int = 0
@@ -11,8 +11,8 @@ def init_run_logging(seed: str):
     global current_run_log_file
     global current_run_log_count
     current_run_log_count = 0
-    current_run_log_file = "runs\\" + dt + "--" + seed
-    with open(ROOT_DIR + "\\logs\\" + current_run_log_file + ".log", 'x') as file:
+    current_run_log_file = "runs/" + dt + "--" + seed
+    with open(ROOT_DIR + "/logs/" + current_run_log_file + ".log", 'x') as file:
         file.close()
 
 
@@ -29,12 +29,12 @@ def log_to_run(message: str):
 
 
 def log(message, filename="default"):
-    f = open(ROOT_DIR + "\\logs\\" + filename + ".log", "a")
+    f = open(ROOT_DIR + "/logs/" + filename + ".log", "a")
     f.write(message + "\n")
     f.close()
 
 
 def init_log(filename="default"):
-    with open(ROOT_DIR + "\\logs\\" + filename + ".log", 'r+') as file:
+    with open(ROOT_DIR + "/logs/" + filename + ".log", 'r+') as file:
         file.truncate(0)
         file.close()
