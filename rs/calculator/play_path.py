27928821dc9d7f18df3082532cca0d4b7092e785
Logan <loganbentley@gmail.com>
2023-04-04 21:02:20 +0200

    Implement BFS for path detection with maximum path limit

--- rs/calculator/play_path.py
+++ rs/calculator/play_path.py
@@ -30,3 +30,36 @@ def get_paths(path: PlayPath, paths: dict[str, PlayPath]):
         new_plays.append(play)
         new_path: PlayPath = PlayPath(new_plays, new_state)
         get_paths(new_path, paths)
+
+
+def get_paths_bfs(state: HandState, max_path_count: int):
+    explored_paths: dict[str, PlayPath] = {}
+    unexplored_paths = [PlayPath([], pickle_deepcopy(state))]
+
+    while len(explored_paths) < max_path_count:
+        # as long as there are unexplored paths, keep progressing
+        if len(unexplored_paths) == 0:
+            break
+        path = unexplored_paths.pop()
+
+        # if we've already seen something similar, don't continue with this path
+        path_state = path.state.get_state_hash()
+        if path_state in explored_paths:
+            continue
+        # otherwise keep it
+        explored_paths[path_state] = path
+
+        # and then queue up all the next states that could happen
+        if path.state.amount_to_discard:
+            plays = path.state.get_discards()
+        else:
+            plays = path.state.get_plays()
+
+        for play in plays:
+            new_state: HandState = pickle_deepcopy(path.state)
+            new_state.transform_from_play(play, is_first_play=not path.plays)
+            new_plays: List[Play] = path.plays.copy()
+            new_plays.append(play)
+            unexplored_paths.append(PlayPath(new_plays, new_state))
+
+    return explored_paths
