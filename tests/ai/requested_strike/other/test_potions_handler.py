b724fd4a92115c8003f7b03c0a401390e8816be9
Typhi <robertaiking@hotmail.com>
2023-05-11 21:12:27 +0200

    Add a little more potions test coverage

--- tests/ai/requested_strike/other/test_potions_handler.py
+++ tests/ai/requested_strike/other/test_potions_handler.py
@@ -1,7 +1,8 @@
 import unittest
 
 from ai.requested_strike.rs_test_handler_fixture import RsTestHandlerFixture
-from rs.ai.requested_strike.handlers.potions_handler import PotionsEliteHandler
+from rs.ai.requested_strike.handlers.potions_handler import PotionsEliteHandler, PotionsBossHandler
+from rs.ai.requested_strike.handlers.smart_battle_handler import SmartBattleHandler
 
 
 class PotionsHandlerTestCase(RsTestHandlerFixture):
@@ -16,6 +17,14 @@ class PotionsHandlerTestCase(RsTestHandlerFixture):
     def test_potions_reptomancer(self):
         self.handler = PotionsEliteHandler
         self.execute_handler_tests('/other/potions_reptomancer.json', ['wait 30', 'potion use 1 3', 'wait 30'])
+
+    def test_boss_potions_handler(self):
+        self.handler = PotionsBossHandler
+        self.execute_handler_tests('/other/potions_boss.json', ['wait 30', 'potion use 0 0', 'wait 30'])
+
+    def test_do_not_use_potion(self):
+        self.handler = SmartBattleHandler
+        self.execute_handler_tests('/other/potions_boss_disliked_potion.json', ['play 4 0'])
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/ai/shivs_and_giggles/other/test_potions_handler.py
@@ -0,0 +1,20 @@
+import unittest
+
+from ai.shivs_and_giggles.sg_test_handler_fixture import SgTestHandlerFixture
+from rs.ai.shivs_and_giggles.handlers.potions_handler import PotionsBossHandler, PotionsEliteHandler
+from rs.ai.shivs_and_giggles.handlers.smart_battle_handler import SmartBattleHandler
+
+
+class PotionsHandlerTestCase(SgTestHandlerFixture):
+
+    def test_boss_potions_handler(self):
+        self.handler = PotionsBossHandler
+        self.execute_handler_tests('/other/potions_boss.json', ['wait 30', 'potion use 0 0', 'wait 30'])
+
+    def test_do_not_use_potion(self):
+        self.handler = SmartBattleHandler
+        self.execute_handler_tests('/other/potions_boss_disliked_potion.json', ['play 4 0'])
+
+
+if __name__ == '__main__':
+    unittest.main()
