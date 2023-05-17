e26c650eb68142dc669b25c195ca1781ea9959a7
Typhi <robertaiking@hotmail.com>
2023-05-17 19:14:48 +0200

    Make campfire a little cleaner

--- rs/ai/shivs_and_giggles/handlers/campfire_handler.py
+++ rs/ai/shivs_and_giggles/handlers/campfire_handler.py
@@ -6,8 +6,8 @@ from rs.machine.command import Command
 from rs.machine.handlers.handler import Handler
 from rs.machine.state import GameState
 
-cards_to_skip_girya_for_upgrading = [
-        'Accuracy',
+high_prio_upgrades = [
+    'Accuracy',
 ]
 
 
@@ -18,28 +18,27 @@ class CampfireHandler(Handler):
 
     def handle(self, state: GameState) -> List[str]:
 
-        choice = "smith"
+        choice = "0"
 
-        if state.has_relic("Girya") \
-                and 'lift' in state.get_choice_list() \
-                and state.get_player_health_percentage() > 0.6 \
+        if 'rest' in state.get_choice_list() \
+                and (state.get_player_health_percentage() <= 0.6 and not pentagraph_will_cover_it(state)) \
+                or state.floor() == 49:
+            choice = "rest"
+
+        elif 'lift' in state.get_choice_list() \
                 and state.get_relic_counter("Girya") < 2 \
-                and not (
-                state.deck.contains_cards(cards_to_skip_girya_for_upgrading) and 'smith' in state.get_choice_list()):
+                and not (state.deck.contains_cards(high_prio_upgrades) and 'smith' in state.get_choice_list()):
             choice = "lift"
 
-        # heal or default
-        if 'smith' not in state.get_choice_list() \
-                or state.get_player_health_percentage() <= 0.6 \
-                or state.floor() == 49:
-            choice = "0"  # 0 is rest if it's available, just whatever if not
-
-        if state.has_relic("Pantograph") \
-                and 'smith' in state.get_choice_list() \
-                and (state.floor() == 15 or state.floor() == 32) \
-                and state.get_player_health_percentage() >= 0.4:
-            choice = "smith"
+        elif 'smith' in state.get_choice_list():
+            choice = 'smith'
 
         if presentation_mode:
             return [p_delay, "choose " + choice, p_delay]
         return ["choose " + choice]
+
+
+def pentagraph_will_cover_it(state: GameState) -> bool:
+    if (state.floor() == 15 or state.floor() == 32) and state.get_player_health_percentage() >= 0.4:
+        return True
+    return False
--- tests/ai/shivs_and_giggles/other/test_campfire_handler.py
+++ tests/ai/shivs_and_giggles/other/test_campfire_handler.py
@@ -6,16 +6,16 @@ class CampfireHandlerTestCase(SgTestHandlerFixture):
     handler = CampfireHandler
 
     def test_rest(self):
-        self.execute_handler_tests('/campfire/campfire_rest.json', ['choose 0'])
+        self.execute_handler_tests('/campfire/campfire_rest.json', ['choose rest'])
 
     def test_smith(self):
         self.execute_handler_tests('/campfire/campfire_smith.json', ['choose smith'])
 
     def test_rest_pantograph_boss(self):
-        self.execute_handler_tests('/campfire/campfire_rest_pantograph_boss.json', ['choose 0'])
+        self.execute_handler_tests('/campfire/campfire_rest_pantograph_boss.json', ['choose rest'])
 
     def test_rest_pantograph_not_boss(self):
-        self.execute_handler_tests('/campfire/campfire_rest_pantograph_not_boss.json', ['choose 0'])
+        self.execute_handler_tests('/campfire/campfire_rest_pantograph_not_boss.json', ['choose rest'])
 
     def test_smith_pantograph_boss(self):
         self.execute_handler_tests('/campfire/campfire_smith_pantograph_boss.json', ['choose smith'])
@@ -27,10 +27,13 @@ class CampfireHandlerTestCase(SgTestHandlerFixture):
         self.execute_handler_tests('/campfire/campfire_girya_lift.json', ['choose lift'])
 
     def test_rest_despite_girya(self):
-        self.execute_handler_tests('/campfire/campfire_girya_rest.json', ['choose 0'])
+        self.execute_handler_tests('/campfire/campfire_girya_rest.json', ['choose rest'])
 
     def test_smith_despite_girya_because_deck(self):
         self.execute_handler_tests('/campfire/campfire_girya_smith_because_deck.json', ['choose smith'])
 
     def test_smith_despite_girya_because_counter(self):
         self.execute_handler_tests('/campfire/campfire_girya_smith_because_counter.json', ['choose smith'])
+
+    def test_default_because_nothing_matches(self):
+        self.execute_handler_tests('/campfire/campfire_default_because_options_blocked.json', ['choose 0'])
