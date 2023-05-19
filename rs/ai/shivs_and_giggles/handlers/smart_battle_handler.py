314500f349441e558a04755834b8f942cf2a927f
Typhi <robertaiking@hotmail.com>
2023-05-19 03:27:30 +0200

    Tentative different formatting for comparator selection
    Though in the mid-term we might want them to declare what they can handle themselves instead of doing it in this file

--- rs/ai/shivs_and_giggles/handlers/smart_battle_handler.py
+++ rs/ai/shivs_and_giggles/handlers/smart_battle_handler.py
@@ -18,22 +18,33 @@ class SmartBattleHandler(Handler):
         return state.has_command(Command.PLAY) or state.current_action() == "DiscardAction"
 
     def handle(self, state: GameState) -> List[str]:
-        comparator = GeneralSilentComparator()
 
-        if state.has_monster("Gremlin Nob"):
-            comparator = GremlinNobSilentComparator()
-        if state.has_monster("Sentry") and len(list(filter(lambda m: not m['is_gone'], state.get_monsters()))) == 3:
-            comparator = ThreeSentriesSilentComparator()
-        if state.has_monster("Lagavulin") \
-                and state.combat_state()['turn'] <= 2 \
-                and (state.deck.contains_type(CardType.POWER)
-                     or state.deck.contains_cards(["Terror", "Terror+"])
-                     or state.has_relic("Warped Tongs")
-                     or state.has_relic("Ice Cream")) \
-                and not state.game_state()['room_type'] == "EventRoom":
-            comparator = WaitingLagavulinSilentComparator()
-        if state.floor() == 33 or state.floor() == 50:
+        big_fight = state.floor() == 33 or state.floor() == 50
+
+        gremlin_nob_is_present = state.has_monster("Gremlin Nob")
+
+        three_sentries_are_alive = state.has_monster("Sentry") \
+                                   and len(list(filter(lambda m: not m['is_gone'], state.get_monsters()))) == 3
+
+        lagavulin_is_sleeping = state.has_monster("Lagavulin") \
+                                and state.combat_state()['turn'] <= 2 \
+                                and not state.game_state()['room_type'] == "EventRoom"
+
+        lagavulin_is_worth_delaying = state.deck.contains_type(CardType.POWER) \
+                                      or state.deck.contains_cards(["Terror", "Terror+"]) \
+                                      or state.has_relic("Warped Tongs") \
+                                      or state.has_relic("Ice Cream")
+
+        if big_fight:
             comparator = BigFightSilentComparator()
+        elif gremlin_nob_is_present:
+            comparator = GremlinNobSilentComparator()
+        elif three_sentries_are_alive:
+            comparator = ThreeSentriesSilentComparator()
+        elif lagavulin_is_sleeping and lagavulin_is_worth_delaying:
+            comparator = WaitingLagavulinSilentComparator()
+        else:
+            comparator = GeneralSilentComparator()
 
         actions = get_best_battle_action(state, comparator, 11_000)
         if actions:
