8564a68830c2b37c6ed7fe75e2965492406ed043
Typhi <robertaiking@hotmail.com>
2023-06-07 01:00:53 +0200

    Move tests around: Astrolabe

---
+++ tests/common/test_astrolabe_handler.py
@@ -0,0 +1,10 @@
+from common.co_test_handler_fixture import CoTestHandlerFixture
+from rs.common.handlers.common_astrolabe_handler import CommonAstrolabeHandler
+
+
+class TestAstrolabeHandler(CoTestHandlerFixture):
+    handler = CommonAstrolabeHandler
+
+    def test_picks_three(self):
+        self.execute_handler_tests('relics/boss_reward_astrolabe.json',
+                                   ['choose 5', 'wait 30', 'choose 2', 'wait 30', 'choose 3', 'wait 30'])
