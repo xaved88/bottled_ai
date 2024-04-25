bae99fd6c33092f521835c56754c1c27f7dd450f
Logan Bentley <loganbentley@gmail.com>
2024-04-26 00:40:01 +0200

    Remove duplicate comparators

--- rs/ai/pwnder_my_orbs/handlers/battle_handler.py
+++ rs/ai/pwnder_my_orbs/handlers/battle_handler.py
@@ -1,8 +1,5 @@
 from rs.ai.pwnder_my_orbs.comparators.pmo_big_fight_comparator import PmoBigFightComparator
 from rs.ai.pwnder_my_orbs.comparators.pmo_general_comparator import PmoGeneralComparator
-from rs.ai.pwnder_my_orbs.comparators.pmo_gremlin_nob_comparator import PmoGremlinNobComparator
-from rs.ai.pwnder_my_orbs.comparators.pmo_three_sentry_comparator import PmoThreeSentriesComparator
-from rs.ai.pwnder_my_orbs.comparators.pmo_waiting_lagavulin_comparator import PmoWaitingLagavulinComparator
 from rs.common.handlers.common_battle_handler import BattleHandlerConfig, CommonBattleHandler
 from rs.machine.handlers.handler import Handler
 
@@ -11,9 +8,6 @@ def get_pmo_battle_handler() -> Handler:
     config = BattleHandlerConfig()
     config.big_fight_floors = [16, 33, 50]
     config.big_fight_comparator = PmoBigFightComparator
-    config.gremlin_nob_comparator = PmoGremlinNobComparator
-    config.three_sentries_comparator = PmoThreeSentriesComparator
-    config.waiting_lagavulin_comparator = PmoWaitingLagavulinComparator
     config.general_comparator = PmoGeneralComparator
 
     return CommonBattleHandler(config=config)
