5548154e318110046b5d970960d2b8cfede8abef
Typhi <robertaiking@hotmail.com>
2024-05-08 23:52:42 +0200

    Watcher tweaks

--- rs/ai/peaceful_pummeling/config.py
+++ rs/ai/peaceful_pummeling/config.py
@@ -1,18 +1,16 @@
 CARD_REMOVAL_PRIORITY_LIST = ['strike', 'strike+', 'defend', 'defend+']
 
 DESIRED_CARDS_FOR_DECK: dict[str, int] = {
-    'tranquility': 3,
-    'crescendo': 3,
+    'tranquility': 4,
+    'crescendo': 4,
     'collect': 1,
-    'brilliance': 1,
-    'pressure points': 1,
+    'pressure points': 4,
     'like water': 2,
     'carve reality': 1,
     'wallop': 1,
     'windmill strike': 1,
     'battle hymn': 1,
     'reach heaven': 1,
-    'deva form': 1,
     'signature move': 1,
     'talk to the hand': 1,
     'swivel': 1,
@@ -21,11 +19,9 @@ DESIRED_CARDS_FOR_DECK: dict[str, int] = {
     'sands of time': 1,
     'rushdown': 1,
     'tantrum': 2,
-    'fasting': 1,
     'wave of the hand': 1,
     'perseverance': 1,
     'wheel kick': 1,
-    'wreath of flame': 1,
     'crush joints': 1,
     'sash whip': 1,
     'follow-up': 1,
@@ -37,12 +33,8 @@ DESIRED_CARDS_FOR_DECK: dict[str, int] = {
     'indignation': 1,
     'mental fortress': 2,
     'fear no evil': 2,
-    'halt': 2,
-    'inner peace': 2,
+    'inner peace': 1,
     'flurry of blows': 2,
-    'prostrate': 2,
-    'worship': 2,
-    'pray': 1,
 }
 
 DESIRED_CARDS_FROM_POTIONS: dict[str, int] = {
