a7cf3e16b425c520ecf80c10a8cb8705f5f482dd
Typhi <robertaiking@hotmail.com>
2024-05-31 23:37:53 +0200

    Add Unawakened awareness to hating Bias after watching 2 runs in a row die to Bias being too early

--- rs/ai/pwnder_my_orbs/comparators/pmo_big_fight_comparator.py
+++ rs/ai/pwnder_my_orbs/comparators/pmo_big_fight_comparator.py
@@ -1,10 +1,15 @@
 from rs.ai.pwnder_my_orbs.comparators.pmo_general_comparator import default_comparisons, PmoGeneralComparator
+from rs.calculator.enums.power_id import PowerId
 from rs.common.comparators.common_general_comparator import move_in_comparison_list, add_to_comparison_list
 from rs.common.comparators.core.comparisons import *
 
 
 def hate_bias(best: CA, challenger: CA) -> Optional[bool]:
-    if challenger.total_monster_health_percent() < 0.60:
+    unawakened_present = False
+    for mon in challenger.state.monsters:
+        if mon.powers.get(PowerId.UNAWAKENED, 0):
+            unawakened_present = True
+    if challenger.total_monster_health_percent() < 0.60 and (not unawakened_present or challenger.total_monster_health_percent() < 0.20):
         return None  # when they've lost more than 60% we don't hate bias anymore
     return None if best.player_bias() == challenger.player_bias() \
         else challenger.player_bias() < best.player_bias()
--- tests/ai/pwnder_my_orbs/handlers/test_battle_handler.py
+++ tests/ai/pwnder_my_orbs/handlers/test_battle_handler.py
@@ -13,6 +13,16 @@ class BattleHandlerTestCase(PmoTestHandlerFixture):
     def test_keep_calculating_even_if_multi_evoke_killed_everyone(self):
         self.execute_handler_tests('battles/with_orbs/multi_evoke_kills_last_enemy.json', ['play 1'])
 
+    def test_hate_bias_if_unawakened_present(self):
+        self.execute_handler_tests(
+            'battles/specific_comparator_cases/big_fight/pmo_big_fight_hate_bias_because_unawakened_present.json',
+            ['end'])
+
+    def test_allow_bias_if_unawakened_present_and_really_low(self):
+        self.execute_handler_tests(
+            'battles/specific_comparator_cases/big_fight/pmo_big_fight_allow_bias_because_unawakened_really_low.json',
+            ['play 1'])
+
 
 if __name__ == '__main__':
     unittest.main()
