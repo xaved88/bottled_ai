30b3398860d363e32d760b3746040524267e2c46
Logan Bentley <loganbentley@gmail.com>
2024-08-09 23:49:05 +0200

    Add test coverage for executor exhausts

--- tests/calculator/test_calculator_executor.py
+++ tests/calculator/test_calculator_executor.py
@@ -1,7 +1,7 @@
 import unittest
 
-from rs.calculator.executor import get_discard_commands
-from rs.calculator.battle_state import PLAY_DISCARD
+from rs.calculator.executor import get_discard_commands, get_exhaust_commands
+from rs.calculator.battle_state import PLAY_DISCARD, PLAY_EXHAUST
 
 
 class CalculatorExecutorTestCase(unittest.TestCase):
@@ -27,5 +27,29 @@ class CalculatorExecutorTestCase(unittest.TestCase):
         plays = [(2, PLAY_DISCARD), (1, -1), (3, PLAY_DISCARD)]
         # when
         result = get_discard_commands(plays)
+        # then
+        self.assertEqual(["choose 2", "confirm", "wait 30"], result)
+
+    def test_simple_exhaust_command_case(self):
+        # given
+        plays = [(0, PLAY_EXHAUST), (0, PLAY_EXHAUST), (0, PLAY_EXHAUST)]
+        # when
+        result = get_exhaust_commands(plays)
+        # then
+        self.assertEqual(["choose 0", "choose 1", "choose 2", "confirm", "wait 30"], result)
+
+    def test_complex_exhaust_command_case(self):
+        # given
+        plays = [(2, PLAY_EXHAUST), (1, PLAY_EXHAUST), (3, PLAY_EXHAUST)]
+        # when
+        result = get_exhaust_commands(plays)
+        # then
+        self.assertEqual(["choose 2", "choose 1", "choose 5", "confirm", "wait 30"], result)
+
+    def test_partial_exhaust_command_case(self):
+        # given
+        plays = [(2, PLAY_EXHAUST), (1, -1), (3, PLAY_EXHAUST)]
+        # when
+        result = get_exhaust_commands(plays)
         # then
         self.assertEqual(["choose 2", "confirm", "wait 30"], result)
